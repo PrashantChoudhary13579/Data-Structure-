af0b424d780e4a7240a15d6cd13e5d56ad98f372
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2024-10-30 22:44:53 +0530

    learning tree

---
+++ 30 October/creatingTree.cpp
@@ -0,0 +1,149 @@
+// Here i am writing this code on 30 October in which i am creating a tree and inserting the values in it as in the format of the tree.
+
+#include <iostream>
+#include <string>
+using namespace std;
+struct node
+{
+    int data;
+    node *parent;
+    node *left;
+    node *right;
+    node (int value)
+    {
+        data = value;
+        parent = NULL;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// Making the Create function
+/*
+struct node *CreateNode(node *p)
+{
+    if (p == NULL)
+    {
+        int value;
+        cout << "Enter the node value " << endl;
+        cin >> value;
+        p->data = value;
+        p->left = NULL;
+        p->right = NULL;
+        CreateNode(p);
+    }
+
+    int value;
+    cout << "Enter the value you want to insert" << endl;
+    cin >> value;
+
+    // struct node *s = (struct node *)malloc(sizeof(struct node));
+    struct node * s = new node (value);
+
+    // Now asking in which side u want to insert either left or right
+    string choose;
+    cout << "Enter the value either left/right (l/r)" << endl;
+    cin >> choose;
+
+
+    if (choose == "l")
+    {
+        if (p->left != NULL)
+        {
+            CreateNode(p->left);
+        }
+        else
+        {
+            s->left = NULL;
+            s->right = NULL;
+            s->data = value;
+            s->parent = p;
+            CreateNode(s);
+        }
+    }
+    else
+    {
+        if (p->right != NULL)
+        {
+            CreateNode(p->right);
+        }
+        else
+        {
+            s->left = NULL;
+            s->right = NULL;
+            s->data = value;
+            s->parent = p;
+            CreateNode(s);
+        }
+    }
+    return;
+}
+*/
+void InsertNode (node * &p , int value, string choose)
+{
+    if (choose =="y")
+    {
+        if (p->left == NULL)
+        {
+            p->left = new node (value);
+            p->left ->parent = p;
+        }
+        else{
+            cout<<"Left child already exists. Try with a different node."<<endl;
+        }
+        
+    }
+    else if (choose =="n")
+    {
+        if (p->right == NULL)
+        {
+            p->right = new node (value);
+            p->right->parent = p;
+        
+        }
+        else
+        {
+            cout<<"Right child already exists . Try again with a different node."<<endl;
+        }
+        
+    }
+    else 
+    {
+        cout<<"Invalid choice . Enter 'y' or 'n' ."<<endl;
+    }
+}
+void CreateNode (node * & root)
+{
+    if (root == NULL)
+    {
+        int value;
+        cout<<"Enter the value of the root element"<<endl;
+        cin>>value;
+        root = new node (value);
+        return;
+    }
+    int value;
+    cout<<"Enter the value you want to insert :"<<endl;
+    cin>>value;
+
+    string choose;
+    cout<<"Enter where to insert as left (y) or (n)";
+    cin>>choose;
+
+    InsertNode(root, value, choose);
+    
+}
+int main()
+{
+    struct node *root = NULL;
+    int numnodes;
+    cout<<"Enter the number of nodes to insert : ";
+    cin>>numnodes;
+    for (int i = 0; i < numnodes; i++)
+    {
+         CreateNode(root);
+    }
+    
+    return 0;
+   
+}
