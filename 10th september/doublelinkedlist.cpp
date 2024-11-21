f5174e25411c371f6f0f302a4087e371a87ff9df
Prashant Choudhary <prashantchoudhary13579@gmail.com>
2024-11-22 02:31:21 +0530

    Update doublelinkedlist.cpp
    adding other additional features too

--- 10th september/doublelinkedlist.cpp
+++ 10th september/doublelinkedlist.cpp
@@ -1,83 +1,184 @@
 #include<iostream>
 using namespace std;
-// making the structure 
-struct node{
+
+// making the structure
+struct node {
     int data;
-    node * next ;
-    node * prev ;
-    node(int d)
-    {
+    node *next;
+    node *prev;
+    node(int d) {
         this->data = d;
-        this->next = prev =nullptr;
+        this->next = prev = nullptr;
     }
 };
-// making a traverse function in forward dir
-void traversing_forward (node * head)
-{
-    node * temp = head;
-    while (temp!= nullptr)
-    {
-        cout<<temp->data<<"  ";
+
+// Traversal forward direction
+void traversing_forward(node *head) {
+    node *temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << "  ";
         temp = temp->next;
     }
-    cout<<endl;
+    cout << endl;
 }
-// making a traverse function in a backward dir
-void traversing_backward(node * tail)
-{
-    node * temp = tail;
-    while (temp != nullptr)
-    {
-        cout<< temp->data <<"  ";
+
+// Traversal backward direction
+void traversing_backward(node *tail) {
+    node *temp = tail;
+    while (temp != nullptr) {
+        cout << temp->data << "  ";
         temp = temp->prev;
     }
-    cout<<endl;
+    cout << endl;
 }
-// finding the length of the double linked list
-int length(node * head )
-{
-    int count=0;
-    node * temp = head;
-    while (temp!= nullptr)
-    {
+
+// Finding the length of the doubly linked list
+int length(node *head) {
+    int count = 0;
+    node *temp = head;
+    while (temp != nullptr) {
         count++;
-        temp = temp->next; 
+        temp = temp->next;
     }
     return count;
 }
-node * insertion_AT_beginning(node * head , int d)
-{
-    // creating a new node
-    node * node1 = new node (d);
-    // adding at the starting position
+
+// Insertion at the beginning
+node* insertion_AT_beginning(node *head, int d) {
+    node *node1 = new node(d);
     node1->next = head;
-    head->prev = node1;
+    if (head != nullptr) {
+        head->prev = node1;
+    }
     return node1;
 }
-node * insertion_AT_end(node * head, int d)
-{
-    // creating a new node
-    node * node1 = new node (d);
-    // adding at the end
-    node * temp = head;
-    while (temp->next != nullptr)
-    {
-       temp = temp->next;
+
+// Insertion at the end
+node* insertion_AT_end(node *head, int d) {
+    node *node1 = new node(d);
+    if (head == nullptr) {
+        return node1;
+    }
+    node *temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
     }
     temp->next = node1;
     node1->prev = temp;
     return head;
 }
-node * insertion_At_position(node * head, int pos , int d)
-{
-    
+
+// Insertion at any position
+node* insertion_At_position(node *head, int pos, int d) {
+    if (pos < 1) {
+        cout << "Invalid Position" << endl;
+        return head;
+    }
+
+    node *temp = new node(d);
+    if (pos == 1) {
+        temp->next = head;
+        if (head != nullptr) {
+            head->prev = temp;
+        }
+        return temp;
+    }
+
+    node *p = head;
+    int i = 1;
+    while (p != nullptr && i < pos - 1) {
+        p = p->next;
+        i++;
+    }
+
+    if (p == nullptr) {
+        cout << "Position out of bound" << endl;
+        delete temp;
+        return head;
+    }
+
+    temp->next = p->next;
+    temp->prev = p;
+    if (p->next != nullptr) {
+        p->next->prev = temp;
+    }
+    p->next = temp;
+
+    return head;
 }
 
-int main()
-{
-    node * first = new node (10);
-    node * second = new node (20);
-    node * third = new node (30);
+// Deletion at the beginning
+node* deleting_from_beginning(node *head) {
+    if (head == nullptr) return nullptr;
+    node *temp = head;
+    head = head->next;
+    if (head != nullptr) {
+        head->prev = nullptr;
+    }
+    delete temp;
+    return head;
+}
+
+// Deletion at the end
+node* deleting_from_end(node *head) {
+    if (head == nullptr) return nullptr;
+    node *temp = head;
+    while (temp->next != nullptr) {
+        temp = temp->next;
+    }
+    if (temp->prev != nullptr) {
+        temp->prev->next = nullptr;
+    } else {
+        head = nullptr;
+    }
+    delete temp;
+    return head;
+}
+
+// Deletion at any position
+node* deleting_from_position(node *head, int pos) {
+    if (pos < 1 || head == nullptr) {
+        cout << "Invalid Position" << endl;
+        return head;
+    }
+
+    if (pos == 1) {
+        node *temp = head;
+        head = head->next;
+        if (head != nullptr) {
+            head->prev = nullptr;
+        }
+        delete temp;
+        return head;
+    }
+
+    node *p = head;
+    int i = 1;
+    while (p != nullptr && i < pos) {
+        p = p->next;
+        i++;
+    }
+
+    if (p == nullptr) {
+        cout << "Position out of bound" << endl;
+        return head;
+    }
+
+    if (p->prev != nullptr) {
+        p->prev->next = p->next;
+    }
+    if (p->next != nullptr) {
+        p->next->prev = p->prev;
+    }
+
+    delete p;
+    return head;
+}
+
+int main() {
+    node *first = new node(10);
+    node *second = new node(20);
+    node *third = new node(30);
 
     first->prev = nullptr;
     first->next = second;
@@ -86,17 +187,37 @@ int main()
     third->prev = second;
     third->next = nullptr;
 
-    // insertion at the starting
-    node * node1 = insertion_AT_beginning(first,40);
+    // Insertion at the beginning
+    node *node1 = insertion_AT_beginning(first, 40);
     traversing_forward(node1);
-    //insertion at end
-    node * node2 = insertion_AT_end(node1,50);
-    traversing_forward(node2);
-    // finding the length
-    int l = length(first);
-    cout<<"Length of the linkedlist = "<<l<<endl;
-    // traversing
-    traversing_forward( first);
-    traversing_backward( third);
 
+    // Insertion at the end
+    node *node2 = insertion_AT_end(node1, 50);
+    traversing_forward(node2);
+
+    // Finding the length
+    int l = length(first);
+    cout << "Length of the doubly linked list = " << l << endl;
+
+    // Traversing forward and backward
+    traversing_forward(first);
+    traversing_backward(third);
+
+    // Insertion at a specific position
+    node *node3 = insertion_At_position(node2, 3, 60);
+    traversing_forward(node3);
+
+    // Deletion at the beginning
+    node *node4 = deleting_from_beginning(node3);
+    traversing_forward(node4);
+
+    // Deletion at the end
+    node *node5 = deleting_from_end(node4);
+    traversing_forward(node5);
+
+    // Deletion at a specific position
+    node *node6 = deleting_from_position(node5, 2);
+    traversing_forward(node6);
+
+    return 0;
 }
