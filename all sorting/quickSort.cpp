dc1ef9f89df3146d248cd944a803cf7e67ad7091
PrashantChoudhary13579 <143925781+PrashantChoudhary13579@users.noreply.github.com>
2024-11-27 17:36:34 +0530

    Quick sort algorithm

--- all sorting/quickSort.cpp
+++ all sorting/quickSort.cpp
@@ -7,3 +7,37 @@
 5.Recursively apply the same process to the two partitioned sub-arrays
 6.The recursion stops when there is only one element left in the sub-array 
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+int partition(vector<int>& arr, int low, int high) {
+
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
+}
+void quickSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+      
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+int main() {
+    vector<int> arr = {10, 7, 8, 9, 1, 5};
+    int n = arr.size();
+    quickSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
